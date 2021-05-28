69c19e3cac94229508067d5fd956c66b6b962d1e
GOKUL <gokuljayakumartvm@gmail.com>
2021-05-28 11:05:09 +0530

    destructuring

--- sample1/src/Counter.jsx
+++ sample1/src/Counter.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Counter(props) {
+   const {title,count}=props
+   
     return (
         <div>
-           <h1 >{props.title}{props.count}</h1> 
+           <h1 >{title}{count}</h1> 
         </div>
     )
 }
