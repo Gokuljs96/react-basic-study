d514685bc7e937ea067b7a1b4f3edbd3570bcc1a
GOKUL <gokuljayakumartvm@gmail.com>
2021-05-28 12:56:25 +0530

    index added

--- sample1/src/App.js
+++ sample1/src/App.js
@@ -3,29 +3,28 @@ import Employee from './Employee'
 
 
 function App() {
-  const [count,setCount]=useState(0)
-  const addCount=()=>{
-    setCount(count+1)
+      const [count,setCount]=useState(0)
+      const addCount=()=>{
+      setCount(count+1)
    
-}
+    }
  let emp =[{name:'gokul',age:23},
- {name:'jayakumar',age:53},
- {name:'anu',age:43},
- ]
- return (
+            {name:'jayakumar',age:53},
+            {name:'anu',age:43},
+           ]
+ return(
     <div className='App'>
 
   <button onClick={addCount}>add</button>
     {
-      emp.map((obj)=>{
+      emp.map((obj,index)=>{
         return(
-          <Employee name={obj.name} age={obj.age}/>
+          <Employee key={index} name={obj.name} age={obj.age}/>
         )
       })
 
     }
-   
-    
+  
     </div>
   );
 }
