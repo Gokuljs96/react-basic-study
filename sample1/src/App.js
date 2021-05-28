1151da16bb32766c0bd55fa3be48b0fb3e35caba
GOKUL <gokuljayakumartvm@gmail.com>
2021-05-28 10:57:18 +0530

    second

--- sample1/src/App.js
+++ sample1/src/App.js
@@ -1,18 +1,21 @@
 import {useState} from 'react'
+import Counter from './Counter';
 
 function App() {
   const [count,setCount]=useState(0)
   const addCount=()=>{
     setCount(count+1)
-    console.log(count);
+   
 }
  
  return (
     <div className='App'>
 
   <button onClick={addCount}>add</button>
-    <h1 >counter:{count}</h1>
-   </div>
+    <Counter title='1st Counter' count={count}/>
+   
+    <Counter title='2st Counter' count={count}/>
+    </div>
   );
 }
 
